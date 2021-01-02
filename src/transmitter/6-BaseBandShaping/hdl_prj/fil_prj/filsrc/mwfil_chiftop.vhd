781a213f6d34846f85f7d21ad4c247ddd1e411c5
Wardo82 <edu_andres@hotmail.es>
2021-01-02 14:10:12 +0100

    HIL of base band shaping. .sof file

---
+++ src/transmitter/6-BaseBandShaping/hdl_prj/fil_prj/filsrc/mwfil_chiftop.vhd
@@ -0,0 +1,110 @@
+
+-- ----------------------------------------------
+-- File Name: mwfil_chiftop.vhd
+-- Created:   02-Jan-2021 13:56:03
+-- Copyright  2021 MathWorks, Inc.
+-- ----------------------------------------------
+
+LIBRARY IEEE;
+USE IEEE.std_logic_1164.all;
+USE IEEE.numeric_std.ALL;
+
+
+ENTITY mwfil_chiftop IS 
+PORT (
+      clk                             : IN  std_logic;
+      reset                           : IN  std_logic;
+      din                             : IN  std_logic_vector(7 DOWNTO 0);
+      din_valid                       : IN  std_logic;
+      dout_ready                      : IN  std_logic;
+      simcycle                        : IN  std_logic_vector(15 DOWNTO 0);
+      din_ready                       : OUT std_logic;
+      dout                            : OUT std_logic_vector(7 DOWNTO 0);
+      dout_valid                      : OUT std_logic
+);
+END mwfil_chiftop;
+
+ARCHITECTURE rtl of mwfil_chiftop IS
+
+COMPONENT mwfil_chifcore IS 
+GENERIC (INWORD: integer := 1;
+OUTWORD: integer := 1;
+WORDSIZE: integer := 64;
+HASENABLE: integer := 1
+);
+PORT (
+      clk                             : IN  std_logic;
+      reset                           : IN  std_logic;
+      din                             : IN  std_logic_vector(7 DOWNTO 0);
+      din_valid                       : IN  std_logic;
+      dout_ready                      : IN  std_logic;
+      simcycle                        : IN  std_logic_vector(15 DOWNTO 0);
+      dut_dout                        : IN  std_logic_vector(79 DOWNTO 0);
+      din_ready                       : OUT std_logic;
+      dout                            : OUT std_logic_vector(7 DOWNTO 0);
+      dout_valid                      : OUT std_logic;
+      dut_din                         : OUT std_logic_vector(47 DOWNTO 0);
+      dut_enable                      : OUT std_logic
+);
+END COMPONENT;
+
+COMPONENT bb_shaping_wrapper IS 
+PORT (
+      clk                             : IN  std_logic;
+      enb                             : IN  std_logic;
+      reset                           : IN  std_logic;
+      din                             : IN  std_logic_vector(47 DOWNTO 0);
+      dout                            : OUT std_logic_vector(79 DOWNTO 0)
+);
+END COMPONENT;
+
+  SIGNAL dut_din                          : std_logic_vector(47 DOWNTO 0); -- std48
+  SIGNAL dut_dout                         : std_logic_vector(79 DOWNTO 0); -- std80
+  SIGNAL dut_clkenb                       : std_logic; -- boolean
+  SIGNAL dut_reset                        : std_logic; -- boolean
+  SIGNAL inphase                          : std_logic_vector(17 DOWNTO 0); -- std18
+  SIGNAL inphase_tmp                      : std_logic_vector(17 DOWNTO 0); -- std18
+  SIGNAL quadrature                       : std_logic_vector(17 DOWNTO 0); -- std18
+  SIGNAL quadrature_tmp                   : std_logic_vector(17 DOWNTO 0); -- std18
+  SIGNAL i_filtered                       : std_logic_vector(37 DOWNTO 0); -- std38
+  SIGNAL i_filtered_tmp                   : std_logic_vector(37 DOWNTO 0); -- std38
+  SIGNAL zero0                            : std_logic_vector(1 DOWNTO 0); -- std2
+  SIGNAL q_filtered                       : std_logic_vector(37 DOWNTO 0); -- std38
+  SIGNAL q_filtered_tmp                   : std_logic_vector(37 DOWNTO 0); -- std38
+  SIGNAL zero1                            : std_logic_vector(1 DOWNTO 0); -- std2
+  SIGNAL tmpconcat                        : std_logic_vector(79 DOWNTO 0); -- std80
+
+BEGIN
+
+u_mwfil_chifcore: mwfil_chifcore 
+GENERIC MAP (INWORD => 6,
+OUTWORD => 10,
+WORDSIZE => 8,
+HASENABLE => 1
+)
+PORT MAP(
+        clk                  => clk,
+        reset                => reset,
+        din                  => din,
+        din_valid            => din_valid,
+        din_ready            => din_ready,
+        dout                 => dout,
+        dout_valid           => dout_valid,
+        dout_ready           => dout_ready,
+        simcycle             => simcycle,
+        dut_din              => dut_din,
+        dut_dout             => dut_dout,
+        dut_enable           => dut_clkenb
+);
+
+u_dut: bb_shaping_wrapper 
+PORT MAP(
+        clk                  => clk,
+        enb                  => dut_clkenb,
+        reset                => reset,
+        din                  => dut_din,
+        dout                 => dut_dout
+);
+
+
+END;
---
+++ src/transmitter/6-BaseBandShaping/hdl_prj/hdlsrc/base_band_shaping_model/bb_shaping_compile.do
@@ -0,0 +1,4 @@
+vlib work
+vcom  FIR_Interpolation.vhd
+vcom  FIR_Interpolation1.vhd
+vcom  bb_shaping.vhd
